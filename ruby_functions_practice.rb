f340f7d50e9bbe9912c87408fd23ca2166628266
Gregor Cox <gregorcox@gmail.com>
2018-05-22 16:57:45 +0100

    number to full month name function works

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,4 +29,33 @@ end
 def add_string_as_number(number1, number2)
   return number1.to_i + number2.to_i
 end
-  
+
+def number_to_full_month_name(month_number)
+  case month_number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else return "That is not a month"
+  end
+end
